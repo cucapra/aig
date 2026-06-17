7862e73ed714de6bd28462df4547eca6b0665722
Modi Goldstein-Rosenfeld <modertool999@gmail.com>
2026-06-17 13:48:39 -0400

    tighten api surface and aig id type

--- src/aig_graph.rs
+++ src/aig_graph.rs
@@ -1,9 +1,12 @@
-pub type NodeId = u32;
+use std::ops::{Index, IndexMut};
 
-pub const INVERSION_MASK: NodeId = 0b0000_0000_0000_0000_0000_0000_0000_0001;
-pub const NODE_ID_MASK: NodeId = 0b1111_1111_1111_1111_1111_1111_1111_1110;
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct NodeId(u32);
 
-pub const INPUT_NODE_MARKER: NodeId = NODE_ID_MASK;
+const INVERSION_MASK: u32 = 0b0000_0000_0000_0000_0000_0000_0000_0001;
+const NODE_ID_MASK: u32 = 0b1111_1111_1111_1111_1111_1111_1111_1110;
+
+const INPUT_NODE_MARKER: NodeId = NodeId(NODE_ID_MASK);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct AigNode {
@@ -11,37 +14,39 @@ pub struct AigNode {
     right: NodeId,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct AigGraph {
     graph: Vec<AigNode>,
 }
 
-pub fn is_inverted(id: NodeId) -> bool {
-    (id & INVERSION_MASK) != 0
-}
+impl NodeId {
+    pub fn is_inverted(self) -> bool {
+        (self.0 & INVERSION_MASK) != 0
+    }
 
-pub fn regular(id: NodeId) -> NodeId {
-    id & NODE_ID_MASK
-}
+    pub fn regular(self) -> Self {
+        Self(self.0 & NODE_ID_MASK)
+    }
 
-pub fn invert(id: NodeId) -> NodeId {
-    id ^ INVERSION_MASK
-}
+    pub fn invert(self) -> Self {
+        Self(self.0 ^ INVERSION_MASK)
+    }
 
-pub fn node_index(id: NodeId) -> usize {
-    (regular(id) >> 1) as usize
-}
+    fn index(self) -> usize {
+        (self.regular().0 >> 1) as usize
+    }
 
-pub fn make_node_id(index: usize) -> NodeId {
-    (index as NodeId) << 1
+    fn from_index(index: usize) -> Self {
+        Self((index as u32) << 1)
+    }
 }
 
 impl AigNode {
-    pub fn new(left: NodeId, right: NodeId) -> Self {
+    fn new(left: NodeId, right: NodeId) -> Self {
         Self { left, right }
     }
 
-    pub fn new_input() -> Self {
+    fn new_input() -> Self {
         Self {
             left: INPUT_NODE_MARKER,
             right: INPUT_NODE_MARKER,
@@ -56,16 +61,12 @@ impl AigNode {
         self.right
     }
 
-    pub fn set_left(&mut self, left: NodeId) {
-        self.left = left;
-    }
-
-    pub fn set_right(&mut self, right: NodeId) {
-        self.right = right;
-    }
-
     pub fn is_input(&self) -> bool {
-        regular(self.left) == INPUT_NODE_MARKER && regular(self.right) == INPUT_NODE_MARKER
+        self.left.regular() == INPUT_NODE_MARKER && self.right.regular() == INPUT_NODE_MARKER
+    }
+
+    pub fn is_and(&self) -> bool {
+        !self.is_input()
     }
 }
 
@@ -83,8 +84,8 @@ impl AigGraph {
     }
 
     pub fn add_input(&mut self) -> NodeId {
-        let index: usize = self.graph.len();
-        let id: NodeId = make_node_id(index);
+        let index = self.graph.len();
+        let id = NodeId::from_index(index);
 
         self.graph.push(AigNode::new_input());
 
@@ -92,47 +93,25 @@ impl AigGraph {
     }
 
     pub fn add_and(&mut self, left: NodeId, right: NodeId) -> NodeId {
-        let index: usize = self.graph.len();
-        let id: NodeId = make_node_id(index);
+        let index = self.graph.len();
+        let id = NodeId::from_index(index);
 
         self.graph.push(AigNode::new(left, right));
 
         id
     }
+}
 
-    pub fn get_node(&self, id: NodeId) -> Option<&AigNode> {
-        self.graph.get(node_index(id))
+impl Index<NodeId> for AigGraph {
+    type Output = AigNode;
+
+    fn index(&self, id: NodeId) -> &Self::Output {
+        &self.graph[id.index()]
     }
+}
 
-    pub fn get_node_mut(&mut self, id: NodeId) -> Option<&mut AigNode> {
-        self.graph.get_mut(node_index(id))
-    }
-
-    pub fn set_node(&mut self, id: NodeId, left: NodeId, right: NodeId) -> Result<(), String> {
-        let node = self
-            .graph
-            .get_mut(node_index(id))
-            .ok_or_else(|| format!("No node exists for NodeId {}", id))?;
-
-        node.set_left(left);
-        node.set_right(right);
-
-        Ok(())
-    }
-
-    pub fn is_input(&self, id: NodeId) -> bool {
-        self.get_node(id).is_some_and(AigNode::is_input)
-    }
-
-    pub fn is_and(&self, id: NodeId) -> bool {
-        self.get_node(id).is_some_and(|node| !node.is_input())
-    }
-
-    pub fn left_child(&self, id: NodeId) -> Option<NodeId> {
-        self.get_node(id).map(AigNode::left)
-    }
-
-    pub fn right_child(&self, id: NodeId) -> Option<NodeId> {
-        self.get_node(id).map(AigNode::right)
+impl IndexMut<NodeId> for AigGraph {
+    fn index_mut(&mut self, id: NodeId) -> &mut Self::Output {
+        &mut self.graph[id.index()]
     }
 }
