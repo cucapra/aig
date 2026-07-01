a487fcbb09bcc3ae246c3bbba507c20fd425bc45
Adrian Sampson <adrian@radbox.org>
2026-07-01 15:39:50 -0400

    An efficient ASCII line parser
    This is a standalone utility (not yet integrated into the parsers) for
    handling text-file lines consisting of lists of integers. It has two
    main advantages over what the ASCII AIGER parser currently does:
    
    * It parses each line in a single pass, from left to right, without
      allocating anything. So we save the time creating and destroying the
      `Vec`s required for `String::split_whitespace`.
    * It operates on bytes instead of Rust's UTF-8-encoded `String`s.
      Because we're only looking for ASCII numerals and whitespace, the full
      generality of Unicode string handling isn't helping us much.

--- src/aiger/mod.rs
+++ src/aiger/mod.rs
@@ -133,3 +133,119 @@ impl Literals {
         }
     }
 }
+
+/// A utility for parsing integers from lines in text files.
+#[derive(Default)]
+pub struct LineParser {
+    pub buf: Vec<u8>,
+    pub pos: usize,
+}
+
+impl LineParser {
+    pub fn new(buf: Vec<u8>) -> Self {
+        Self { buf, pos: 0 }
+    }
+
+    /// Empty the line buffer.
+    pub fn clear(&mut self) {
+        self.buf.clear();
+        self.pos = 0;
+    }
+
+    /// Get all the data remaining in the line buffer.
+    pub fn rest(&self) -> &[u8] {
+        &self.buf[self.pos..]
+    }
+
+    /// Read a line from a text-file stream. Return a flag indicating EOF.
+    pub fn read_line<R: BufRead>(&mut self, reader: &mut R) -> std::io::Result<bool> {
+        self.clear();
+        reader.read_until(b'\n', &mut self.buf)?;
+        Ok(self.buf.is_empty())
+    }
+
+    fn peek(&self) -> Option<u8> {
+        self.buf.get(self.pos).copied()
+    }
+
+    fn skip(&mut self) {
+        debug_assert!(self.pos < self.buf.len());
+        self.pos += 1;
+    }
+
+    fn pop_if(&mut self, pred: impl Fn(u8) -> bool) -> Option<u8> {
+        if let Some(byte) = self.peek()
+            && pred(byte)
+        {
+            self.skip();
+            Some(byte)
+        } else {
+            None
+        }
+    }
+
+    /// Consume a single ASCII integer from the current point in the line buffer.
+    pub fn parse_int(&mut self) -> Option<usize> {
+        let mut out: Option<usize> = None;
+
+        while let Some(byte) = self.pop_if(|b| b.is_ascii_digit()) {
+            let value = byte - b'0';
+            out = Some(match out {
+                Some(old) => old * 10 + (value as usize),
+                None => value as usize,
+            });
+        }
+
+        out
+    }
+
+    /// Advance the line buffer to consume a sequence of ASCII whitespace.
+    pub fn skip_whitespace(&mut self) {
+        while self.pop_if(|b| b.is_ascii_whitespace()).is_some() {}
+    }
+
+    /// Consume a sequence of whitespace-separated integers.
+    pub fn parse_ints<const N: usize>(&mut self) -> Option<[usize; N]> {
+        // Sadly, `std::array::try_from_fn` is unstable. We fake it by setting a
+        // flag on parsing errors and, on failure, wastefully continuing to
+        // construct an array that we will eventually throw away.
+        let mut failed = false;
+        let arr = std::array::from_fn(|_| {
+            self.skip_whitespace();
+            match self.parse_int() {
+                Some(i) => i,
+                None => {
+                    failed = true;
+                    0
+                }
+            }
+        });
+        if failed { None } else { Some(arr) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn just_int() {
+        let mut parser = LineParser::new("42".as_bytes().to_vec());
+        assert_eq!(parser.parse_int(), Some(42));
+        assert!(parser.rest().is_empty());
+    }
+
+    #[test]
+    fn int_with_stuff() {
+        let mut parser = LineParser::new("42x".as_bytes().to_vec());
+        assert_eq!(parser.parse_int(), Some(42));
+        assert_eq!(parser.rest().len(), 1);
+    }
+
+    #[test]
+    fn two_ints() {
+        let mut parser = LineParser::new("42 27 x".as_bytes().to_vec());
+        assert_eq!(parser.parse_ints(), Some([42, 27]));
+        assert_eq!(parser.rest().len(), 2);
+    }
+}
