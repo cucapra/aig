5f7bc2575182a66b2731d53a8f15a2c3d6ec573b
Modi Goldstein-Rosenfeld <modertool999@gmail.com>
2026-07-10 15:00:12 -0400

    implement kevin's edits (2)

--- src/graph/eval.rs
+++ src/graph/eval.rs
@@ -8,38 +8,42 @@ impl AigGraph {
         if id.is_false() {
             0
         } else if id.is_true() {
-            1
+            Value::MAX
         } else if id.is_inverted() {
-            1 - self.eval(id.regular(), values)
+            !self.eval(id.regular(), values)
         } else {
             let node = &self[id];
 
-            if node.is_input() || node.is_latch() {
-                *values.get(&id).unwrap()
-            } else if node.is_and() {
+            if let Some(value) = values.get(&id) {
+                *value
+            } else {
+                assert!(
+                    node.is_and(),
+                    "The node is not an AND, so it cannot be calculated and a value needs to be provided"
+                );
+
                 let left = self.eval(node.left(), values);
                 let right = self.eval(node.right(), values);
                 left & right
-            } else {
-                panic!("invalid AIG node classification for {:?}", id)
             }
         }
     }
 
     /// Simulate the whole circuit for several time steps.
     ///
-    /// Each Env in `input_vectors` is one time step's input assignment.
+    /// Each Vec<Value> in `input_vectors` is one time step's input assignment.
+    /// Values are ordered to match the graph's inputs.
     ///
     /// Returns one Vec<Value> per time step, containing the output values
     /// for that time step.
     ///
     /// example input_vectors:
     /// [
-    /// { a: 1, b: 0 },   // time 0
-    /// { a: 0, b: 0 },   // time 1
-    /// { a: 1, b: 1 },   // time 2
+    /// vec![1, 0],   // time 0
+    /// vec![0, 0],   // time 1
+    /// vec![1, 1],   // time 2
     /// ]
-    pub fn simulate(&self, input_vectors: &[Env]) -> Vec<Vec<Value>> {
+    pub fn simulate(&self, input_vectors: &[Vec<Value>]) -> Vec<Vec<Value>> {
         // format: output_trace[time_step][output_index]
         let mut output_trace = Vec::new();
 
@@ -55,16 +59,8 @@ impl AigGraph {
 
         // Each iteration is one time step / one clock cycle.
         for input_values in input_vectors {
-
             // write this time step's inputs into current
-            for &input_id in &self.inputs {
-                let value = *input_values.get(&input_id).unwrap();
-                // maybe todo: have a helper function to generate an input_vectors
-                // that includes this invariant check, also maybe includes a simple way
-                // to build an input_vector with inputs that do not change over time
-                //i.e., every entry in input_vectors is the same
-                assert!(value == 0 || value == 1);
-
+            for (&input_id, &value) in self.inputs.iter().zip(input_values.iter()) {
                 current.insert(input_id, value);
             }
 
@@ -90,14 +86,9 @@ impl AigGraph {
             }
 
             // update all latches together (i.e., clock tick)
-            // could this be done with threads???
-            for &latch_id in &self.latches {
-                let next_value = *next.get(&latch_id).unwrap();
-                current.insert(latch_id, next_value);
-            }
+            current = next;
         }
 
         output_trace
     }
 }
-
