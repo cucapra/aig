db797335390e038d386a421f6370e3b3e4077c8e
Kevin Läufer <laeufer@cornell.edu>
2026-07-16 21:13:03 -0400

    eval: add unittest of a 8-bit counter

--- src/graph/eval.rs
+++ src/graph/eval.rs
@@ -92,3 +92,125 @@ impl AigGraph {
         output_trace
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Commands::Parse;
+    use crate::graph::AigBuilder;
+
+    fn and(g: &mut AigBuilder, a: NodeId, b: NodeId) -> NodeId {
+        g.add_and_optimized(a, b)
+    }
+
+    fn or(g: &mut AigBuilder, a: NodeId, b: NodeId) -> NodeId {
+        // a || b = not(not(a || b)) = not(not(a) && not(b))
+        and(g, a.invert(), b.invert()).invert()
+    }
+
+    fn xor(g: &mut AigBuilder, a: NodeId, b: NodeId) -> NodeId {
+        // a xor b = (a || b) && !(a && b)
+        let a_or_b = or(g, a, b);
+        let a_and_b = and(g, a, b);
+        and(g, a_or_b, a_and_b.invert())
+    }
+
+    fn xor3(g: &mut AigBuilder, a: NodeId, b: NodeId, c: NodeId) -> NodeId {
+        let a_xor_b = xor(g, a, b);
+        xor(g, a_xor_b, c)
+    }
+
+    fn majority(g: &mut AigBuilder, a: NodeId, b: NodeId, c: NodeId) -> NodeId {
+        // majority(a,b,c) = ((a xor b) and c) or (a and b)
+        //                 = or(and(a, b), or(and(a,c), and(b,c)))
+        let a_xor_b = xor(g, a, b);
+        let a_and_b = and(g, a, b);
+        let a_xor_b_and_c = and(g, a_xor_b, c);
+        or(g, a_xor_b_and_c, a_and_b)
+    }
+
+    fn half_adder(g: &mut AigBuilder, a: NodeId, b: NodeId) -> (NodeId, NodeId) {
+        let sum = xor(g, a, b);
+        let carry = and(g, a, b);
+        (carry, sum)
+    }
+
+    fn full_adder(g: &mut AigBuilder, a: NodeId, b: NodeId, c: NodeId) -> (NodeId, NodeId) {
+        let sum = xor3(g, a, b, c);
+        let carry = majority(g, a, b, c);
+        (carry, sum)
+    }
+
+    fn add(g: &mut AigBuilder, a: &[NodeId], b: &[NodeId]) -> Vec<NodeId> {
+        assert_eq!(a.len(), b.len());
+        assert!(a.len() >= 1);
+        let (c0, s0) = half_adder(g, a[0], b[0]);
+        let mut sum = vec![s0];
+        let mut carry = c0;
+        for (&a, &b) in a.iter().skip(1).zip(b.iter().skip(1)) {
+            let (c, s) = full_adder(g, a, b, carry);
+            sum.push(s);
+            carry = c;
+        }
+        sum
+    }
+
+    fn zero(bits: u32) -> Vec<NodeId> {
+        vec![NodeId::FALSE; bits as usize]
+    }
+
+    fn one(bits: u32) -> Vec<NodeId> {
+        assert!(bits > 0);
+        let mut out = zero(bits);
+        out[0] = NodeId::TRUE;
+        out
+    }
+
+    fn set_latch_inputs(g: &mut AigBuilder, latches: &[NodeId], next: &[NodeId]) {
+        assert_eq!(latches.len(), next.len());
+        for (&l, &n) in latches.iter().zip(next.iter()) {
+            g.node(l).set_latch_input(n);
+        }
+    }
+
+    fn make_counter(g: &mut AigBuilder, bits: u32) {
+        let counter: Vec<_> = (0..bits).map(|_| g.add_latch(NodeId::FALSE)).collect();
+        let one = one(bits);
+        let counter_next = add(g, &counter, &one);
+        set_latch_inputs(g, &counter, &counter_next);
+        for &c in &counter {
+            g.add_output(c);
+        }
+    }
+
+    fn read_bit_vector(values: &[Value], offset: usize, width: u32) -> u64 {
+        assert!(width <= 64);
+        let width = width as usize;
+        assert!(values.len() >= width + offset);
+        values
+            .iter()
+            .skip(offset)
+            .enumerate()
+            .map(|(idx, &value)| ((value as u64) & 1) << idx)
+            .reduce(|a, b| a | b)
+            .unwrap_or(0)
+    }
+
+    #[test]
+    fn test_sim_counter() {
+        let bits = 8;
+        let cycles = (1 << bits) + 1;
+
+        let counter_mask = (1 << bits) - 1;
+        let mut g = AigBuilder::new();
+        make_counter(&mut g, bits);
+        let g = g.build();
+        let inputs = vec![vec![]; cycles];
+        let result = g.simulate(&inputs);
+        for (step, values) in result.iter().enumerate() {
+            let count = read_bit_vector(values, 0, bits);
+            assert_eq!(count, (step & counter_mask) as u64);
+            // println!("step: {}, count: {}", step, count);
+        }
+    }
+}
